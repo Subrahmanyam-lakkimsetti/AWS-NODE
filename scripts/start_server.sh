8f382e1c23607c793223c3b22b84bb5785e1ef3d
LAKKIMSETTI N V S D Ramasubrahmanyam <119732527+Subrahmanyam-lakkimsetti@users.noreply.github.com>
2025-07-14 12:57:45 +0530

    updated start

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Starting Node.js server..."
-cd /home/ec2-user/my-node-app   # ✅ correct folder from appspec.yml
-pkill node || true              # ✅ optional: stop any existing node process
+cd /home/ec2-user/my-node-app
+
+# Kill any previously running node process (optional)
+sudo pkill node || true
+
+# Start the app in the background and log output
 nohup node app.js > app.log 2>&1 &
