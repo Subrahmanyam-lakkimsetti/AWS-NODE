d1ce6624b17fe34c680b66a62d96949c922b08d5
LAKKIMSETTI N V S D Ramasubrahmanyam <119732527+Subrahmanyam-lakkimsetti@users.noreply.github.com>
2025-07-14 12:45:22 +0530

    updated start server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Starting Node.js server..."
-cd /home/ec2-user/AWS-NODE
-pkill node || true
-nohup node app.js > output.log 2>&1 &
+cd /home/ec2-user/my-node-app   # ✅ correct folder from appspec.yml
+pkill node || true              # ✅ optional: stop any existing node process
+nohup node app.js > app.log 2>&1 &
