b407848c5c731387ff642ccdd233d3122998c62c
LAKKIMSETTI N V S D Ramasubrahmanyam <119732527+Subrahmanyam-lakkimsetti@users.noreply.github.com>
2025-07-14 12:58:50 +0530

    updated api's

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('here is the ci/cd pratical example');
+  res.send('hello there this is the end point');
 });
 
 app.listen(3000, () => {
